f23cd9b84ea226dc61c2cfd15737658b043f5d99
Rococcoon <mar.lm@protonmail.com>
2026-08-18 16:42:45 -0700

    feat: init inventory service microservice

---
+++ inventory-service/src/main/java/com/crtlaltkeeb/inventory_service/InventoryServiceApplication.java
@@ -0,0 +1,13 @@
+package com.crtlaltkeeb.inventory_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InventoryServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InventoryServiceApplication.class, args);
+	}
+
+}
